bcd53acb4c09a2e108ed24a64ef0c20988910a1b
Robert Lord <lord@users.noreply.github.com>
2025-06-20 17:19:07 -0400

    tmp: progress

--- src/uuseg.ml
+++ src/uuseg.ml
@@ -45,13 +45,12 @@ type t = Seg : 'a * 'a segmenter -> t
 type ret = Uuseg_base.ret
 
 let create boundary =
-  let boundary = (boundary :> boundary) in
   match boundary with
-  | `Grapheme_cluster -> Seg (boundary, Uuseg_grapheme_cluster.create (), Grapheme_cluster)
-  | `Word -> Seg (boundary, Uuseg_word.create (), Word)
-  | `Sentence -> Seg (boundary, Uuseg_sentence.create (), Sentence)
-  | `Line_break -> Seg (boundary, Uuseg_line_break.create (), Line_break)
-  | `Custom (C c) -> Seg (boundary, c.create (), Custom c)
+  | `Grapheme_cluster -> Seg (Uuseg_grapheme_cluster.create (), Grapheme_cluster)
+  | `Word -> Seg (Uuseg_word.create (), Word)
+  | `Sentence -> Seg (Uuseg_sentence.create (), Sentence)
+  | `Line_break -> Seg (Uuseg_line_break.create (), Line_break)
+  | `Custom (C c) -> Seg (c.create (), Custom c)
 
 let boundary (Seg (_, seg)) =
   match seg with
@@ -59,9 +58,9 @@ let boundary (Seg (_, seg)) =
   | Word -> `Word
   | Sentence -> `Sentence
   | Line_break -> `Line_break
-  | Custom c -> `Custom c
+  | Custom c -> `Custom (C c)
 
-let add (Seg (_, s, seg)) add =
+let add (Seg (s, seg)) add =
   match seg with
   | Grapheme_cluster -> Uuseg_grapheme_cluster.add s add
   | Word -> Uuseg_word.add s add
@@ -69,19 +68,19 @@ let add (Seg (_, s, seg)) add =
   | Line_break -> Uuseg_line_break.add s add
   | Custom {add = f; _} -> f s add
 
-let mandatory (Seg (_, s, seg)) =
+let mandatory (Seg (s, seg)) =
  match seg with
   | Grapheme_cluster | Word | Sentence -> mandatory_default s
   | Line_break -> Uuseg_line_break.mandatory s
   | Custom {mandatory; _} -> mandatory s
 
-let copy (Seg (b, s, seg)) =
+let copy (Seg (s, seg)) =
   match seg with
-  | Grapheme_cluster -> Seg (b, Uuseg_grapheme_cluster.copy s, seg)
-  | Word -> Seg (b, Uuseg_word.copy s, seg)
-  | Sentence -> Seg (b, Uuseg_sentence.copy s, seg)
-  | Line_break -> Seg (b, Uuseg_line_break.copy s, seg)
-  | Custom {copy; _} -> Seg (b, copy s, seg)
+  | Grapheme_cluster -> Seg (Uuseg_grapheme_cluster.copy s, seg)
+  | Word -> Seg (Uuseg_word.copy s, seg)
+  | Sentence -> Seg (Uuseg_sentence.copy s, seg)
+  | Line_break -> Seg (Uuseg_line_break.copy s, seg)
+  | Custom {copy; _} -> Seg (copy s, seg)
 
 let pp_ret = Uuseg_base.pp_ret
 
@@ -92,4 +91,11 @@ let custom ?(mandatory = mandatory_default) ~name ~create ~copy ~add () =
 
 let err_exp_await = Uuseg_base.err_exp_await
 let err_ended = Uuseg_base.err_ended
-let equal _ _ = raise (Invalid_argument "Uuseg.equal: not implemented")
+
+let equal s1 s2 =
+  match s1, s2 with
+  | Seg (s1, Grapheme_cluster), Seg (s2, Grapheme_cluster) -> Some (Uuseg_grapheme_cluster.equal s1 s2)
+  | Seg (s1, Word), Seg (s2, Word) -> Some (Uuseg_word.equal s1 s2)
+  | Seg (s1, Sentence), Seg (s2, Sentence) -> Some (Uuseg_sentence.equal s1 s2)
+  | Seg (s1, Line_break), Seg (s2, Line_break) -> Some (Uuseg_line_break.equal s1 s2)
+  | _ -> None
